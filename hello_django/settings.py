319871cd3c55fb9f632a7e0a35521c3febee09dd
genaro <genagevo@gmail.com>
2024-09-15 21:08:58 -0300

    adding CSRF trusted origin

--- hello_django/settings.py
+++ hello_django/settings.py
@@ -131,6 +131,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "mediafiles"
 
-CSRF_TRUSTED_ORIGINS = ["http://localhost:1337"]
+CSRF_TRUSTED_ORIGINS = ["http://vps-3503468-x.dattaweb.com:1337"]
 
 ALLOWED_HOSTS = ["*"]
